612d6bcb832476783499bf04895a35abd8be1f5b
Buburry <Natapon7934@gmail.com>
2023-12-19 16:34:26 +0700

    lab10.3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,41 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream source;
+    source.open("score.txt");
+
+    int  numdata = 0;
+    double mean = 0, stdand = 0,value;
+    string textline;
+
+    while (getline(source, textline)) {
+        value = atof(textline.c_str());
+        mean += value;
+        numdata++;
+    }
+    source.clear(); // Clear the end-of-file flag
+    source.seekg(0, ios::beg); // Reset file pointer to the beginning
+
+    mean = mean/ numdata;
+
+    while (getline(source, textline)) {
+        value = atof(textline.c_str());
+        stdand += pow((value - mean), 2);
+    }
+
+    stdand = sqrt(stdand / numdata);
+
+    source.close();
+
+    cout << "Number of data = " << numdata << endl;
+    cout << setprecision(3);
+    cout << "Mean = " <<  mean << endl;
+    cout << "Standard deviation = " <<  stdand << endl;
+
+    return 0;
+}
